420690036faeb58b76a444095b93d62b63f9ed8a
尹庆文 <Yinqingwen@gmail.com>
2021-12-06 18:07:38 +0800

    20211206 18:07

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return "Welcome to use! haha!"
+    return "Welcome to use!"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
