aeb946749c29e70c681e4abf8cb848f269a27160
尹庆文 <Yinqingwen@gmail.com>
2021-12-06 16:50:08 +0800

    20211206 16:50

---
+++ app.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+"""
+Author: yinqingwen
+Email:yinqingwen@gmail.com
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return "Welcome to use!"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
